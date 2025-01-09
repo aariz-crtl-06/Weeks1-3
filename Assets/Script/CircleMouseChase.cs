7c0766830785de85b768187afc2f20b18d5acc54
Aariz Abdul <abdulaar@shernet.sheridancollege.ca>
2025-01-09 11:18:12 -0500

    Made square move right to the right and made the circle follow mouse

---
+++ Assets/Script/CircleMouseChase.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CircleMouseChase : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePos;
+  
+        
+
+    }
+}
