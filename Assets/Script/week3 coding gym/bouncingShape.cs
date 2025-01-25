719b1b4992f8b2bebe48c2f7992347413c5b9c3d
DELL18\aariz <abdulaar@sheridancollege.ca>
2025-01-25 13:25:02 -0500

    Did coding gym week 3 tirckshot exercise

--- Assets/Script/week3 coding gym/bouncingShape.cs
+++ Assets/Script/week3 coding gym/bouncingShape.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -8,48 +9,68 @@ public class bouncingShape : MonoBehaviour
 
     public float speed = 0.5f;
     public float t;
-    float jeeter = 5;
+    public float t2;
+    [Range(0,1)]
+    public bool isPressed = false;
     public AnimationCurve jump;
+    public AnimationCurve spin;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        t = 0;
+        t2 = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 pos = transform.position;
+        bounce();
+    }
+void bounce()
+    {
+
+        Vector3  pos = transform.position;
+        Vector3 rot = transform.eulerAngles;
         pos.x += speed * Time.deltaTime;
 
-        Vector2 sqaureInScreenSpace = Camera.main.WorldToScreenPoint(pos);
+        Vector3 shapeInScreenSpace = Camera.main.WorldToScreenPoint(pos);
 
-        if (sqaureInScreenSpace.x < -10 || sqaureInScreenSpace.x > Screen.width)
+        if (shapeInScreenSpace.x < -10 || shapeInScreenSpace.x > Screen.width)
         {
             speed = speed * -1;
         }
 
-        t += Time.deltaTime;
-
-        if (t > 1)
-        {
-            t = 0;
-        }
-
-
-        transform.position = pos;
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //transform.position = Vector2.one * jump.Evaluate(t);
-            t *= 5;
-            Debug.Log("TEST: ");
+            isPressed = true;
         }
 
-        if(Input.GetKeyUp(KeyCode.Space))
+        if (isPressed)
         {
-            //transform.position = Vector2.one * jump.Evaluate(t);
-            t *= -5;
+            t += Time.deltaTime;
+            t2 += Time.deltaTime;
+            if (t > 1f)
+            {
+                t = 0;
+                isPressed = false;
+            }
+
+            if (t2> 1f)
+            {
+                t2= 0;
+              
+            }
         }
+
+
+        pos.y = jump.Evaluate(t);
+        rot.z = spin.Evaluate(t2);
+        transform.position = pos;
+        transform.eulerAngles = rot;
+   
+
     }
+
+
 }
