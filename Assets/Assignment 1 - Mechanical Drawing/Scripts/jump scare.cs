b1bfe558159ee0c42a8c4f11a043f396a2714e86
DELL18\aariz <abdulaar@sheridancollege.ca>
2025-01-28 22:46:44 -0500

    Added an animation curve to the jumpscare

--- Assets/Assignment 1 - Mechanical Drawing/Scripts/jump scare.cs
+++ Assets/Assignment 1 - Mechanical Drawing/Scripts/jump scare.cs
@@ -6,20 +6,32 @@ using static UnityEditor.PlayerSettings;
 
 public class jumpscare : MonoBehaviour
 {
-    
+
+    public AnimationCurve bounce;
+    [Range(0,1)]
+    public float t;
+
     void Start()
     {
-        
+        t = 0;
     }
 
     
     void Update()
     {
-        
+
+        t+= Time.deltaTime;
+
+        if (t > 1)
+        {
+            t = 0;
+        }
+
 
         if (Input.GetKey(KeyCode.S))
         {
-            transform.localScale = Vector2.one * 2.3f;
+            transform.localScale = Vector2.one * bounce.Evaluate(t)*2.3f;
+
         }
 
         else
